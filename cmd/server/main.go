45a8d3bf334db32d167c29bf314a2b4ea5376516
ham-andres <asd.ray6@gmail.com>
2026-07-18 19:58:24 +0530

    initial commit with server changes in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os/signal"
+	"os"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connectString = "amqp://guest:guest@localhost:5672/"
+	connectStatus, err := amqp.Dial(connectString)
+	if err != nil {
+		log.Fatalf("Connection failed: %v", err)
+	}
+	defer connectStatus.Close()
+	fmt.Println("Connection Succesfull: localhost:5672")
+
+	// wait for ctrl + C 
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("program shutting down and Closing the Connection")
+	return	
+
 }
