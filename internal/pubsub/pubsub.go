8fd521650273a982adc192382562f1d3bc0d1f53
ham-andres <asd.ray6@gmail.com>
2026-09-04 12:31:03 +0530

    in between commit

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"context"
+	"encoding/gob"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -21,6 +22,26 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	err = ch.PublishWithContext(context.Background(),exchange, key, false, false, msg)
 	if err != nil {
 		return fmt.Errorf("channel publish failed: %w", err)
+	}
+	return nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(val)
+	if err != nil {
+		return fmt.Errorf("gob encoder failed: %v", err)
+	}
+
+	msg := amqp.Publishing {
+		ContentType:	"application/gob",
+		Body: buffer.Bytes(),
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, msg)
+	if err != nil {
+		return fmt.Errorf("Channel gob publish failed: %v", err)
 	}
 	return nil
 }
